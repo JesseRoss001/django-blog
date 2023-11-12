79480198f16de377b701110cff69cd3b5d41d813
Jesse Lee Ross <jesseross001@gmail.com>
2023-11-12 20:20:54 +0000

    Generic post view

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,8 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+
     
     class Meta:
         ordering = ["-created_on"]
@@ -35,6 +37,7 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField (default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    challenge = models.FloatField(default="3.0")
 
     class Meta:
         ordering = ["-created_on"]
